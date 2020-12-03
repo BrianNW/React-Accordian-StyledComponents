238d602954210aeb2e0c66f42e41090cf4901e3c
Brian <briantechdom@gmail.com>
2020-12-03 10:08:37 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Accordian from "./components/Accordian";
+
+function App() {
+  return <Accordian />;
+}
+
+export default App;
---
+++ src/components/Accordian.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Data } from "./Data";
+
+const Accordian = () => {
+  return <div>accordian yo</div>;
+};
+
+export default Accordian;
